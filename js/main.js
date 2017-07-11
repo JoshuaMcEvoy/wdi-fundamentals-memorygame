529d14b2e865bee1707d5bd0dd3ecf6cd2a7c40a
Joshua McEvoy <MacEnroy@joshuas-mbp.home>
2017-07-11 17:03:08 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,22 +3,31 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
 
-if (cardsInPlay.length === 2){
-	// if (cardsInPlay[0] === cardsInPlay[1]){
-	// 	alert("You've found a match!");
-	// }
-	// else {
-	// 	alert("Sorry, try again.");
-	// }
+var checkForMatch = function(){
+	
+	// if (cardsInPlay[0] === cardsInPlay[1]) {
+ //      console.log("You found a match!");
+ //  } else {
+ //      console.log("Sorry, try again.");
+ //  }
 
-	cardsInPlay[0] === cardsInPlay[1] ? alert("You've found a match!"):alert("Sorry, try again.");
-
-	//some additional information so git will commmit
+	if (cardsInPlay.length === 2){
+		// if (cardsInPlay[0] === cardsInPlay[1]){
+		// 	alert("You've found a match!");
+		// }
+		// else {
+		// 	alert("Sorry, try again.");
+		// }
+		cardsInPlay[0] === cardsInPlay[1] ? alert("You've found a match!"):alert("Sorry, try again.");
+	}
 }
+
+var flipCard = function(cardId){
+	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
